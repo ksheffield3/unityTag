5e1e155f5a03a5bf8f4f703b6be3e069cd718319
Kelley Sheffield <ksheffield3@gatech.edu>
2013-11-01 15:19:26 -0400

    Initial Commit

---
+++ unityTag/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  unityTag
+//
+//  Created by Kelley Sheffield on 11/1/13.
+//  Copyright (c) 2013 Kelley Sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ unityTag/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  unityTag
+//
+//  Created by Kelley Sheffield on 11/1/13.
+//  Copyright (c) 2013 Kelley Sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
